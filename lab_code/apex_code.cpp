89364ad99c57d1bc05f2b7108d2404f15220a965
kaelbond <gaelocomotor@icloud.com>
2026-03-05 23:15:04 -0800

    Add 12-hour time conversion functions
    Implemented functions to convert minutes and numbers to 12-hour time format.

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -1,30 +1,32 @@
-//
-//
 #include <iostream>
 #include <sstream>
 #include "apex_code.h"
 
 using namespace std;
 
-void minutes_to_12hour_time(stringstream &cin, stringstream &cout) {
-    // Place code for translating minutes to 12 hour time
-    // Pretend this is your main function and you can use cin/cout
-    // as you would in main.
+void minutes_to_12hour_time(stringstream &in, stringstream &out) {
+    long long total_minutes;
+    in >> total_minutes;
 
-    // If you want to print to the console for debugging use std::cout.
-    // Use cout for your final solution.
+    // Calculamos horas (0-11) y minutos
+    long long hours = (total_minutes / 60) % 12;
+    long long minutes = total_minutes % 60;
 
-    // First you want to get user input with cin
-    // Do any needed calculations
-    // Create output with cout
+    out << hours << ":" << minutes << " !";
 }
 
-void numbers_to_12hour_time(stringstream& cin, stringstream& cout)
-{
-    // Place code for translating hours, minutes and seconds to 12 hour time
-    // Pretend this is your main function and you can use cin/cout
-    // as you would in main.
+void numbers_to_12hour_time(stringstream &in, stringstream &out) {
+    double h_in = 0, m_in = 0, s_in = 0;
+    in >> h_in >> m_in >> s_in;
 
-    // If you want to print to the console for debugging use std::cout.
-    // Use cout for your final solution.
+    // Convertimos todo a segundos para manejar decimales
+    double total_seconds = (h_in * 3600.0) + (m_in * 60.0) + s_in;
+    long long total_secs_int = static_cast<long long>(total_seconds);
+
+    // Formato de 12 horas
+    long long h = (total_secs_int / 3600) % 12;
+    long long m = (total_secs_int / 60) % 60;
+    long long s = total_secs_int % 60;
+
+    out << h << ":" << m << "." << s << " !";
 }
